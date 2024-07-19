ad54dc700888b263b0101380871bd0c0bac3b6c2
shrivastavaPriyanshi <143926625+shrivastavaPriyanshi@users.noreply.github.com>
2024-07-19 12:19:45 +0530

    new

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const  { listingSchema } = require("./schema.js");
+const Review = require("./models/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -106,6 +107,20 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
     res.redirect("/listings");
   }));
 
+//Reviews
+//Post Route
+app.post("/listings/:id/reviews",async(req,res) => {
+   let listing = await Listing.findById(req.params.id);
+   let newReview = new Review(req.body.review);
+
+   listing.reviews.push(newReview);
+
+   await newReview.save();
+   await listing.save();
+
+   res.redirect(`/listings/${listing._id}`);
+});
+
 // app.get("/testListing", async (req, res) => {
 //     let sampleListing = new Listing({ 
 //         title: "My New Villa",
@@ -128,7 +143,7 @@ app.use((err,req, res, next) => {
     // res.status(statusCode).send(message);
     res.status(statusCode) .render("error.ejs", {err});
  
-});
+}); 
 app.listen(8080, () => {
     console.log("server is listening to port 8080");
 });
